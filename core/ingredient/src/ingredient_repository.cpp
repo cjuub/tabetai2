14527b61006415697812bba5d23153eaa0b57fca
Robin Linden <dev@robinlinden.eu>
2020-08-12 22:34:27 +0200

    Use constructor inheritance to pull in identical constructor

--- core/ingredient/src/ingredient_repository.cpp
+++ core/ingredient/src/ingredient_repository.cpp
@@ -2,13 +2,6 @@
 
 namespace tabetai2::core::ingredient {
 
-using namespace database;
-
-IngredientRepository::IngredientRepository(std::unique_ptr<Database<Ingredient>> database)
-: Repository(std::move(database)) {
-
-}
-
 std::optional<Ingredient> IngredientRepository::find_by_id(int id) const {
     return m_database->get(id);
 }
