9e15d21941b3f2ed7d03af9126b7325f6bfa7007
cjuub <cjuuub@gmail.com>
2022-01-08 12:37:31 +0100

    Kill WAMP

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -10,13 +10,8 @@ include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 conan_cmake_run(
     REQUIRES
-        boost/1.73.0
-        msgpack/3.2.1
         range-v3/0.10.0
-        websocketpp/0.8.2
         yaml-cpp/0.6.3
-        zlib/1.2.11
-        bzip2/1.0.8
     BASIC_SETUP
     CMAKE_TARGETS
     GENERATORS cmake_find_package
