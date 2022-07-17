2afad30109ff736dc11a113c675487c18449e6bd
cjuub <cjuuub@gmail.com>
2022-07-17 10:54:30 +0200

    Fix GRPC communication when running as app

---
+++ gui/tabetai2_flutter/lib/backend/grpc_channel_factory.dart
@@ -0,0 +1,11 @@
+import 'package:grpc/grpc.dart';
+import 'package:grpc/grpc_connection_interface.dart';
+
+class GrpcChannelFactory {
+  static ClientChannelBase create(String host, int port) {
+    return ClientChannel(host,
+        port: port,
+        options:
+            const ChannelOptions(credentials: ChannelCredentials.insecure()));
+  }
+}
