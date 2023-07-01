6cdabf2027a62dbc76910535bd993e9d843bf629
cjuub <cjuuub@gmail.com>
2023-07-01 17:17:00 +0200

    Update conan to 2.x

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -1,27 +1,3 @@
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(STATUS "Downloading conan.cmake")
-    file(DOWNLOAD
-        "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
-        "${CMAKE_BINARY_DIR}/conan.cmake"
-    )
-endif()
-
-include(${CMAKE_BINARY_DIR}/conan.cmake)
-
-conan_cmake_run(
-    REQUIRES
-        range-v3/0.10.0
-        yaml-cpp/0.6.3
-        grpc/1.42.0
-    BASIC_SETUP
-    CMAKE_TARGETS
-    GENERATORS cmake_find_package
-    BUILD missing
-    PROFILE_AUTO ALL
-)
-
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
-
 find_package(range-v3 REQUIRED)
 target_compile_options(range-v3::range-v3
     INTERFACE
@@ -32,12 +8,13 @@ target_compile_options(range-v3::range-v3
 )
 
 find_package(yaml-cpp REQUIRED)
-target_compile_definitions(yaml-cpp::yaml-cpp
+target_compile_definitions(yaml-cpp
     INTERFACE
         $<$<CXX_COMPILER_ID:MSVC>:
             _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING;
         >
 )
+add_library(yaml-cpp::yaml-cpp ALIAS yaml-cpp)
 
 find_package(gRPC REQUIRED)
 add_library(grpc::grpc++ ALIAS gRPC::grpc++)
