e0e54dbfc9c7de0df5839d39c28f29597fb11daa
cjuub <cjuuub@gmail.com>
2024-02-18 10:35:40 +0100

    Remove Conan support

--- cmake/conan_install.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan_provider.cmake")
-    message(STATUS "Downloading conan_provider.cmake")
-    file(DOWNLOAD
-        "https://github.com/conan-io/cmake-conan/raw/develop2/conan_provider.cmake"
-        "${CMAKE_BINARY_DIR}/conan_provider.cmake"
-    )
-endif()
-
-set(CMAKE_PROJECT_TOP_LEVEL_INCLUDES "${CMAKE_BINARY_DIR}/conan_provider.cmake")
