b81fca3a36f095e0279b217bac551592ab29a282
cjuub <cjuuub@gmail.com>
2024-10-13 10:14:11 +0200

    Fix add recipe not working

--- gui/tabetai2_flutter/lib/application/recipes_view_widget.dart
+++ gui/tabetai2_flutter/lib/application/recipes_view_widget.dart
@@ -93,8 +93,8 @@ class _RecipesViewWidgetState extends State<RecipesViewWidget> implements TopicS
                                             backendClient: widget.backendClient,
                                             ingredientsData: _ingredients,
                                             units: _units,
-                                            recipeIngredientsData: const [],
-                                            steps: const [],
+                                            recipeIngredientsData: [],
+                                            steps: [],
                                             recipeId: "",
                                           )));
                               setState(() {});
